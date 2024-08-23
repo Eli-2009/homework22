78ff49ff85ba5a565e2757ab03a125be72c256b6
Eli-2009 <elieliyarov18@gmail.com>
2024-08-23 23:15:07 +0400

    homework

---
+++ app.js
@@ -0,0 +1,8 @@
+let text = prompt("metn yazin")
+console.log(text.length);
+console.log(text.includes("e"));
+console.log(text.startsWith("code"));
+console.log(text.endsWith("code"));
+console.log(text.split("t").join("####"));
+
+
